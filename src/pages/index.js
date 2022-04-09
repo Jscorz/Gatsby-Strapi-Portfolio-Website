c62f24a9ab0209d930b7714a3768f096131e16bb
Justin Scorzafava <jkscorzafava@gmail.com>
2022-04-09 14:21:57 -0600

    added query for featured projects from strapi

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,8 @@ import Services from "../components/Services"
 import Jobs from "../components/Jobs"
 import Projects from "../components/Projects"
 import Seo from "../components/Seo"
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
+  console.log(data)
   return (
     <>
       <main>
@@ -16,5 +17,31 @@ const IndexPage = () => {
     </>
   )
 }
+
+export const query = graphql`
+  {
+    allStrapiProject(filter: { featured: { eq: true } }) {
+      nodes {
+        description
+        featured
+        github
+        id
+        slug
+        title
+        url
+        stack {
+          id
+          title
+        }
+        image {
+          childImageSharp {
+            gatsbyImageData(layout: CONSTRAINED, placeholder: BLURRED)
+          }
+        }
+      }
+      totalCount
+    }
+  }
+`
 
 export default IndexPage
