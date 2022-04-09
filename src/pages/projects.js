4cff711eb401bff84ceffc8b96c61055161e1868
Justin Scorzafava <jkscorzafava@gmail.com>
2022-04-09 15:01:55 -0600

    added projects as destructured props to projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,12 +2,47 @@ import React from "react"
 import { graphql } from "gatsby"
 import Projects from "../components/Projects"
 import Seo from "../components/Seo"
-const ProjectsPage = () => {
+
+const ProjectsPage = ({
+  data: {
+    allStrapiProject: { nodes: projects },
+  },
+}) => {
   return (
     <>
-      <h2>projects page</h2>
+      <main>
+        <section className="projects-page">
+          <Projects title="all projects" projects={projects} />
+        </section>
+      </main>
     </>
   )
 }
+
+export const query = graphql`
+  {
+    allStrapiProject(filter: { featured: { eq: true } }) {
+      nodes {
+        description
+        featured
+        github
+        id
+        slug
+        title
+        url
+        stack {
+          id
+          title
+        }
+        image {
+          childImageSharp {
+            gatsbyImageData(layout: CONSTRAINED, placeholder: BLURRED)
+          }
+        }
+      }
+      totalCount
+    }
+  }
+`
 
 export default ProjectsPage
