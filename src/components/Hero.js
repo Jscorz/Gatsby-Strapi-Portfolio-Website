bec86725bbf2b7860654caaaf135f281f8dc974d
Justin Scorzafava <jkscorzafava@gmail.com>
2022-04-09 18:06:58 -0600

    changed hero img to be displayed even on small screens

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,6 +3,7 @@ import { Link } from "gatsby"
 import socialLinks from "../constants/social_links"
 // import heroImg from "../assets/images/hero.svg"
 import { StaticImage } from "gatsby-plugin-image"
+import heroImg from "../assets/images/hero9.svg"
 
 const Hero = () => {
   return (
