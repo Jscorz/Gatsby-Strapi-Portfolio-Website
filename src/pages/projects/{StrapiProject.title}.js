9206648bd243e26e38488e697bacf73142f127dd
Justin Scorzafava <jkscorzafava@gmail.com>
2022-04-10 10:17:18 -0600

    added livedemo and sourcecode buttons on individual project page and changed query to include those props

--- src/pages/projects/{StrapiProject.title}.js
+++ src/pages/projects/{StrapiProject.title}.js
@@ -1,13 +1,23 @@
 import React from "react"
 import { graphql } from "gatsby"
 import SEO from "../../components/Seo"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import { FaGithubSquare, FaShareSquare } from "react-icons/fa"
 
-const ProjectTemplate = ({ pageContext: { title }, data }) => {
+const ProjectTemplate = ({ pageContext: { title, github, url }, data }) => {
   return (
     <>
       <main className="project-template-page">
         <h2>{title}</h2>
         <p>{data.strapiProject.description}</p>
+        <div className="projects-links">
+          <a href={data.strapiProject.github} target="_blank" rel="noreferrer">
+            <FaGithubSquare className="project-icon font-size-higher"></FaGithubSquare>
+          </a>
+          <a href={data.strapiProject.url} target="_blank" rel="noreferrer">
+            <FaShareSquare className="project-icon font-size-higher"></FaShareSquare>
+          </a>
+        </div>
       </main>
     </>
   )
@@ -15,12 +25,14 @@ const ProjectTemplate = ({ pageContext: { title }, data }) => {
 
 export const query = graphql`
   query getSingleProject($title: String) {
-    strapiProject(title: { eq: $title }) {
+    strapiProject(title: { eq: $title }, github: {}) {
       description
       title
       image {
         publicURL
       }
+      github
+      url
     }
   }
 `
